434f3ca9effa2c8414babacc6cfd02fc30f7b360
Brian Toal <btoal@brians-imac.attlocal.net>
2020-04-18 17:33:53 -0700

    Moved non-util related methods back to main program

--- hello.asm
+++ hello.asm
@@ -8,7 +8,17 @@ section .text
 global _start
 
 extern printhello
+extern log_arguments
 
 _start:
+    call log_arguments
     call printhello
     sys_exit 0
+
+printhello:
+    mov rax, mommy
+    sys_write_stdout
+
+    mov rax, fortnite
+    sys_write_stderr
+    ret
--- util.asm
+++ util.asm
@@ -1,17 +1,8 @@
 %include "syscall.asm"
-
-section .data
-    mommy db "mommy pancake",10, 0
-    fortnite db "fortnite dances",10, 0
     
 section .text
 
-global printhello
+global log_arguments
 
-printhello:
-    mov rax, mommy
-    sys_write_stdout
-
-    mov rax, fortnite
-    sys_write_stderr
+log_arguments:
     ret
